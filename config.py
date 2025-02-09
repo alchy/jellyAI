33c43c41bf5565dc6109a5b13c70b3e49f76ece9
Jindrich Nemec <jindrich.nemec@yahoo.com>
2025-02-09 01:18:59 +0100

    change in nn

--- config.py
+++ config.py
@@ -12,8 +12,8 @@ TEXT_ATTENTION_WEIGHT = 0.1
 SAVE_DIR = "./model_checkpoints"
 
 # Training parameters
-BATCH_SIZE = 128
-EPOCHS = 4
+BATCH_SIZE = 8
+EPOCHS = 16
 
 # Path to the directory containing .txt files
 TEXT_DIRECTORY = 'input_text'
