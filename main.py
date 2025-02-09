2cacc179b4a3507b24393f16525b869a24910e11
Jindrich Nemec <jindrich.nemec@yahoo.com>
2025-02-09 02:10:16 +0100

    change in nn

--- main.py
+++ main.py
@@ -27,11 +27,20 @@ def main():
     # Initialize model handler
     model_handler = ModelHandler(text_processor, config.SAVE_DIR)
     
-    # Prepare and train model if needed
+
     if config.TRAINING:
-        # Now we pass just two arguments: text_attention and span_length
+        # Prepare and train model (nearby words)
         X, y = model_handler.prepare_training_data(text_attention, config.TEXT_ATTENTION_SPAN_LENGTH)
         
+        print("\nSample input/output data visualization:")
+        print_aligned_vocabulary_and_array_combo(
+            text_processor.vocabulary_itw, X[0], y[0]
+        )
+        model_handler.train(X, y, config.BATCH_SIZE, config.EPOCHS)
+
+        # Prepare and train model (missing word)
+        y, X = model_handler.prepare_training_data(text_attention, config.TEXT_ATTENTION_SPAN_LENGTH)
+
         print("\nSample input/output data visualization:")
         print_aligned_vocabulary_and_array_combo(
             text_processor.vocabulary_itw, X[0], y[0]
